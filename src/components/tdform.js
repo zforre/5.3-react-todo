0eb699485065c7abfac7074688d95f641f2f7e25
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-02-05 13:23:33 -0500

    builds form component

--- src/components/tdform.js
+++ src/components/tdform.js
@@ -1,27 +1,38 @@
 import React, {Component} from 'react';
-import { render } from '@testing-library/react';
 
 class TdForm extends Component {
-    constructor(props);
-    super(props);
-    this.state = {
-        text: ''
-    }
+
+    constructor(props) {
+        super(props);
+            this.state = {
+                text: ''
+            }
+
+            this.handleInput = this.handleInput.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+        }
+
+        handleInput(event) {
+            this.setState({[event.target.name]: event.target.value});
+        }
+
+        handleSubmit(event) {
+            event.preventDefault();
+            this.props.addTodo(this.state);
+            this.setState({text: ''});
+        }
 
     render() {
         return(
-            
+            <form onSubmit={this.handleSubmit} className='row no-gutters'>
+                <div className='form-group col-6'>
+                    <label htmlFor='text'>Add something to do.</label>
+                    <input className="form-control" type="text" name="text" id="text" value={this.state.text} onChange={this.handleInput} />
+                </div>
+                <button className='btn btn-primary'>Add</button>
+            </form>
         )
     }
 }
 
-
-
-
-
-
-
-
-
-
-export default 
+export default TdForm
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,12 +3,42 @@ import TdForm from './../components/tdform.js'
 import TdList from './../compenents/tdlist.js'
 import './App.css';
 
+const todos = [{id: 0, text: 'Wash the car.', isComplete: false}];
+
 class TdApp extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+      counter: 1
+    }
+
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({todos});
+  }
+  
+  addTodo(todo) {
+    todo.id=this.state.counter;
+    todo.isComplete =false;
+
+    let todos = [this.state.todos];
+    todos.push(todo);
+
+    this.setState((state, props) => ({
+      todos, counter: this.state.counter +1
+    }));
+  }
+
   render(){
     return (
       <div>
         <h1>What do you need to do?</h1>
-        <TdForm />
+        <TdForm addTodo={this.addTodo}/>
         < TdList />
       </div>
     );
